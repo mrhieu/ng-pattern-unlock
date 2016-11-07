694c4fd2994e9da24bb47645f33c0da66be404e6
Hieu Pham <mr_hie@yahoo.com>
2016-11-08 04:10:03 +0700

    feat: oops it works

--- src/components/patternUnlock/pattern.directive.js
+++ src/components/patternUnlock/pattern.directive.js
@@ -10,34 +10,70 @@
       return {
         restrict: 'E',
         templateUrl: 'components/patternUnlock/views/pattern.directive.html',
-        link: function() {
+        link: function(scope, el, attr, ctrl) {
+          // e.touches[0].clientX/clientY
+
+          el.on('mousedown', function(e) {
+            el.on('mousemove', function(e) {
+              // Dragging through the pattern-nodes
+              if (e.target.className.indexOf('pattern-node') !== -1) {
+                ctrl.toggleNode(angular.element(e.target).attr('node-index'), true);
+                scope.$apply();
+              }
+            });
+          });
+
+          el.on('mouseup', function(e) {
+            el.off('mousemove');
+          });
+
+          el.on('click', function(e) {
+            ctrl.reset();
+            scope.$apply();
+          });
 
         },
         controller: PatternCtrl,
-        controllerAs: '$ctrl',
+        controllerAs: 'vm',
       }
     })
 
   function PatternCtrl(PATTERN_OPTIONS) {
-    var $ctrl = this;
+    var vm = this;
 
-    $ctrl.patternSize = PATTERN_OPTIONS.size;
-    $ctrl.nodes = generateNodes(PATTERN_OPTIONS.size);
-    $ctrl.lines = [];
+    vm.patternSize = PATTERN_OPTIONS.size;
+    vm.nodes = generateNodes(PATTERN_OPTIONS.size);
+    vm.lines = [];
+    vm.pattern = [];
 
-    $ctrl.toggleNode = function(index, isActive) {
+    vm.toggleNode = function(index, isActive) {
+      // One node can be used only once
+      if (!vm.nodes[index].active) {
+        vm.pattern.push(index);
+
+        var curLength = vm.pattern.length;
+        if (curLength >=2) {
+          // draw line between the latest 2 nodes
+          vm.drawLine(vm.pattern[curLength - 2], vm.pattern[curLength - 1], PATTERN_OPTIONS.size);
+        }
+      }
+
       if (typeof isActive != 'undefined') {
-        $ctrl.nodes[index].active = isActive
+        vm.nodes[index].active = isActive
       } else {
-        $ctrl.nodes[index].active = $ctrl.nodes[index].active? false: true;
+        vm.nodes[index].active = vm.nodes[index].active? false: true;
       }
     }
 
-    $ctrl.drawLine = function(id1, id2) {
-      $ctrl.lines.push(caculatedLine(id1, id2, PATTERN_OPTIONS.size));
+    vm.drawLine = function(id1, id2, size) {
+      vm.lines.push(caculatedLine(id1, id2, size));
     }
 
-    $ctrl.drawLine(5, 6);
+    vm.reset = function() {
+      vm.nodes = generateNodes(PATTERN_OPTIONS.size);
+      vm.lines = [];
+      vm.pattern = [];
+    }
   }
 
   function generateNodes(size) {
@@ -65,7 +101,7 @@
     var p1 = coordinate(id1, size);
     var p2 = coordinate(id2, size);
 
-    // Almost forgot about Math :< http://www.brainsofsteel.co.uk/post/How-to-Calculate-the-Direction-between-two-Points
+    // Almost forgot about Maths :< http://www.brainsofsteel.co.uk/post/How-to-Calculate-the-Direction-between-two-Points
     var diffx = p1.x - p2.x;
     var diffy = p1.y - p2.y;
     var angle = Math.atan2(diffx, diffy) * 180 /Math.PI;
